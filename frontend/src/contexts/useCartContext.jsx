e6b3efb1446e6114798a4381de5882b82ca12dd8
emerson08alves <emerson08alves@gmail.com>
2024-11-28 03:35:01 -0300

    inserindo alerta de prato adiionado

--- frontend/src/contexts/useCartContext.jsx
+++ frontend/src/contexts/useCartContext.jsx
@@ -1,57 +1,68 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState } from "react";
+import Swal from 'sweetalert2';
 
-const CartContext = createContext()
+const CartContext = createContext();
 
 export function CartProvider({ children }) {
-    const [cartItems, setCartItems] = useState([])
+    const [cartItems, setCartItems] = useState([]);
 
     const addToCart = (itemToAdd) => {
         const checkItemAlready = cartItems.find((cartItem) => {
-            return cartItem._id === itemToAdd._id
-        })
+            return cartItem._id === itemToAdd._id;
+        });
 
-        if(!checkItemAlready) {
-            itemToAdd.quantity = 1
+        if (!checkItemAlready) {
+            itemToAdd.quantity = 1;
 
-            setCartItems([...cartItems, itemToAdd])
-          //  console.log('Item adicionado corretamente')  
-            alert('Prato adicionado ao carrinho com sucesso!') 
+            setCartItems([...cartItems, itemToAdd]);
+            // console.log('Item adicionado corretamente');
+            Swal.fire({
+                title: 'Sucesso!',
+                text: 'Prato adicionado ao carrinho com sucesso!',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            });
+
         } else {
-           // console.log('O item já está no carrinho')   
-            alert('O item já está no carrinho, caso deseje adicionar mais um parto  vá até o carrinho e altere a quantidade.') 
+            // console.log('O item já está no carrinho');
+            Swal.fire({
+                title: 'Atenção!',
+                text: 'O item já está no carrinho, caso deseje adicionar mais um prato vá até o carrinho e altere a quantidade.',
+                icon: 'warning',
+                confirmButtonText: 'OK'
+            });
         }
-    }
+    };
 
     const removeFromCart = (itemId) => {
         const cartItemsSanitized = cartItems.filter((item) => {
-            return item._id !== itemId
-        })
-        
-        setCartItems(cartItemsSanitized)
+            return item._id !== itemId;
+        });
 
-    }
+        setCartItems(cartItemsSanitized);
+    };
 
     const updateCartItems = (items) => {
-        setCartItems(items)
-    }
+        setCartItems(items);
+    };
 
     const clearCart = () => {
-        setCartItems([])
-    }
+        setCartItems([]);
+    };
 
-    return(
+    return (
         <CartContext.Provider value={{ removeFromCart, addToCart, cartItems, updateCartItems, clearCart }}>
             {children}
         </CartContext.Provider>
-    )
+    );
 }
 
 export const useCartContext = () => {
-    const context = useContext(CartContext)
+    const context = useContext(CartContext);
 
-    if(!context) {
-        console.log('você está fora do contexto do carrinho')
+    if (!context) {
+        console.log('você está fora do contexto do carrinho');
     }
 
-    return context
-}
+    return context;
+};
